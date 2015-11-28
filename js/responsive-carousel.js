d179e98e223a6b8171748e3f52fa10d5674aedb9
ResponsiveCarousel <derekchan916@gmail.com>
2015-11-28 12:44:10 -0500

    Add initializer.

--- js/responsive-carousel.js
+++ js/responsive-carousel.js
@@ -1,3 +1,27 @@
+$.Carousel = function (el) {
+  this.$el = $(el);
+  this.$carousel = this.$el.find(".carousel");
+  this.$pages = this.$el.find("ul");
+  this.$items = this.$el.find("li");
+  this.$nav = $("<nav></nav>");
+
+  LARGEMINSIZE = 767;
+  LARGECOUNT = 35;
+  MEDMINSIZE = 480;
+  MEDCOUNT = 20;
+  SMALLCOUNT = 16;
+
+  this.currentWindowSize = null;
+  this.activeIdx = 0;
+
+  this.processWindowSize();
+
+  this.$el.on("swiperight", this.swipeLeftNav.bind(this));
+  this.$el.on("swipeleft", this.swipeRightNav.bind(this));
+  this.$el.on("click", "span", this.clickNav.bind(this));
+  $(window).on("resize", function() { this.processWindowSize()}.bind(this));
+};
+
 $.fn.carousel = function () {
   return this.each(function () {
     new $.Carousel(this);
